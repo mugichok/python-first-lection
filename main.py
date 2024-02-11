444eca59859658bd5f19151eaa672226b6ae58ae
gendfrodit <farengate20017@gmail.com>
2024-02-11 16:00:16 +0300

    first lection

---
+++ main.py
@@ -0,0 +1,83 @@
+# a = 5
+# b = 6
+# c = 'hello'
+
+# print(f"{a} - {b} - {c}")
+
+# a = 5
+# b = 6
+# c = 'hello'
+
+# print("{} - {} - {}".format(a,b,c))
+
+# print('Введите первое число: ')
+# a = int(input())
+# a = int(a)
+# print(a)
+
+# b = int(input('Введите второе число: '))
+# b = int(b)
+# print(b)
+
+# print(a, '+', b, '=', a+b)
+
+
+# c = 1
+# print(c)
+# print(type(c))
+# c = bool(c)
+# print(c)
+# print(type(c))
+
+
+# a = 5.3984935
+# b = 6.29488374748
+# print(round(a*b, 3))
+
+# username = input('Введите имя: ')
+# if username == 'Маша':
+#     print('ура, это Маша')
+# elif username == 'Саша':
+#     print('здорова, сашок')
+# elif username == 'марина':
+#     print('маринОчка, привет')
+# else:
+#     print('Привет, ', username)
+
+# i = 0
+# while i < 5:
+#     i=i+1
+# else:
+#     print("Пожвлуй, хватит)")
+#     print(i)
+
+
+# n = int(input())
+# flag = True
+# i = 2
+# while flag:
+#     if n % i == 0:
+#         flag = False
+#         print(i)
+#     elif i > n//2:
+#         print(n)
+#         flag = False
+#     i+=1
+
+# a = 'qwerty'
+# for i in a:
+#     print(i)
+
+# line = ''
+# for i in range(5):
+#     line = ''
+#     for j in range(5):
+#         line += '+'
+#     print(line)
+
+text = 'У МЕНЯ ПАТАУ'
+# print(len(text))
+# print('меня'in text)
+# print(text.lower())
+# print(text.upper())
+print(text.replace('МЕНЯ','М'))
